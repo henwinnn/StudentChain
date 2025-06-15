91c273b38ef7bd4f27982becc50661b038750800
henwinnn <93514124+henwinnn@users.noreply.github.com>
2025-06-15 12:11:23 +0700

    push abi

---
+++ contracts/contrats.ts
@@ -0,0 +1,9 @@
+
+import { abi } from "@/app/ABI/abi";
+import type { Address } from "viem";
+
+export const stableSwapContract = {
+  address: "0x9ecF5a31cf3F2df152A0b271F28C42f4CC6103b9" as Address,
+  abi: abi ,
+} as const;
+
