e42f5da5b835af624a82dfbf7d73a41ada05863f
henwinnn <93514124+henwinnn@users.noreply.github.com>
2025-06-19 17:46:39 +0700

    change contract address

--- contracts/contracts.ts
+++
@@ -1,9 +0,0 @@
-
-import { abi } from "@/app/ABI/abi";
-import type { Address } from "viem";
-
-export const StudentMasterContract = {
-  address: "0x34ADFc585fd4cD8b7641Ab5F23Eec15431A822c7" as Address,
-  abi: abi,
-} as const;
-
--- contracts/contrats.ts
+++ contracts/contrats.ts
@@ -2,15 +2,15 @@ import { abi } from '@/app/ABI/abi';
 import type { Address } from 'viem';
 
 export const CampusCreditContract = {
-  address: '0x7A7Ffe65546AeFafc77b8bf52d9E944589A1dC6D' as Address,
+  address: '0xfcb5b4b87E3c7716a136b290465A18e93a06fDBa' as Address,
   abi: abi,
 } as const;
 export const StudentIDContract = {
-  address: '0x3dCDC2c2fFf5D6D1C73678BDDdE87F97B3B649a7' as Address,
+  address: '0xb3A86F2982cD02BC4832c02699ae30c6C88F329A' as Address,
   abi: abi,
 } as const;
 
 export const CampusMasterContract = {
-  address: '0xB33B71e0E7E50c509024531666F25dDf455044B0' as Address,
+  address: '0x8ceEde932dB45E8cb6389D2ffa7b09385f3386dB' as Address,
   abi: abi,
 } as const;
